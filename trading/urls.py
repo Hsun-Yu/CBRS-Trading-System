99cf9d6354c07ce3cde426282d5c46c118d8e4f4
HsunYu <leoxz02leo@outlook.com>
2020-05-17 17:52:42 +0800

    Finish login method

--- trading/urls.py
+++ trading/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'/login', views.login, name='login'),
-    url(r'/signup', views.signup, name='signup')
+    url(r'/login', views.loginView, name='loginView'),
+    url(r'/signup', views.signupView, name='signupView')
 ];
--- trading/views.py
+++ trading/views.py
@@ -2,17 +2,37 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import SignUpForm
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
 from .models import PAL, GAA
 from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import login as auth_login
 
 # Create your views here.
 def index(request):
     return render(request, 'trading/index.html')
 
-def login(request):
-    return render(request, 'trading/login.html')
+def loginView(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"{username} Login Success~")
+                return redirect('/')
+            else:
+                messages.info(request, "Password or Username Wrong!!")
+        else:
+            messages.info(request, "Password or Username Wrong!!")
 
-def signup(request):
+    form = AuthenticationForm()
+    return render(request = request, template_name = "trading/login.html", context={"form":form})
+
+def signupView(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
